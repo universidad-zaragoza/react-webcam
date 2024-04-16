f99a1e66fd783587487e599bea714c78eecb6932
adnana03 <801567@unizar.es>
2024-04-16 14:52:07 +0200

    Updates

--- assets/hyphenate-style-name-HOLnhz8K.js
+++
@@ -1,1 +0,0 @@
-var e=/[A-Z]/g,o=/^ms-/,n={};function a(r){return"-"+r.toLowerCase()}function u(r){if(n.hasOwnProperty(r))return n[r];var t=r.replace(e,a);return n[r]=o.test(t)?"-"+t:t}export{u as h};
